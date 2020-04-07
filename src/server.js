beaae0e69299c7ac6037d87e3be0e84d51264069
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-04-07 13:05:36 -0500

    added files

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world from a Node.js app!');
+});
+
+app.get('/secret', (req, res) => {
+  res.send(`mysecret: ${process.env.MYSECRET}`);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is up on 3000');
+});
